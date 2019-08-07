482decd297636bd00b2dfa46827ddc3e45105de7
Shane Weisz <shaneweisz@gmail.com>
2019-08-07 16:19:09 +0200

    Added ARM attribute class with good documentation

---
+++ src/arm_attribute.py
@@ -0,0 +1,31 @@
+class ARM_Attribute:
+    """
+    A class used to represent an attribute of an ARM Entity.
+
+    Attributes
+    ----------
+    name : str
+        The name of the attribute.
+    data_type : str
+        The data type of the attribute - defaulted to anyType.
+    """
+
+    def __init__(self, name, data_type="anyType"):
+        """Attribute constructor - with default data type of 'anyType'."""
+        self.name = name
+        self.data_type = data_type
+
+    def get_name(self):
+        """Getter for name."""
+        return self.name
+
+    def get_data_type(self):
+        """Getter for data type."""
+        return self.data_type
+
+    def __str__(self):
+        """
+        String representation of the attribute - its name and data type.
+        e.g. 'Age (int)'
+        """
+        return "{} ({})".format(self.name, self.data_type)
